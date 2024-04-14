5f58caf176cffc15c79a77a2c140804acc08e5fb
dmitry_lotkov <dmitry.lotkov9@gmail.com>
2024-04-14 17:49:10 +0300

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,23 @@ export class AppComponent implements OnInit{
   }
 
   ngOnInit() {
+    const timeOptions = [];
+
+    for (let hours = 0; hours < 24; hours++) {
+      for (let minutes = 0; minutes < 60; minutes += 30) {
+        const hourFormat = hours < 10 ? `0${hours}` : `${hours}`;
+        const minuteFormat = minutes < 10 ? `0${minutes}` : `${minutes}`;
+        const timeValue = `${hourFormat}:${minuteFormat}`;
+
+        timeOptions.push({
+          label: timeValue,
+          value: timeValue
+        });
+      }
+    }
+
+    console.log(timeOptions);
+
     this.authService.me()
   }
 }
